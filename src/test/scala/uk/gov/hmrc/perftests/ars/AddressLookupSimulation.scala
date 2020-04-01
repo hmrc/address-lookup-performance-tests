35c051f8816d4aad9e4292cab748c0a734cd599b
antony.teoh <antony.teoh@digital.hmrc.gov.uk>
2020-04-01 14:38:29 +0100

    Update configuration for performance test to run ALF journey only

--- src/test/scala/uk/gov/hmrc/perftests/ars/AddressLookupSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/ars/AddressLookupSimulation.scala
@@ -1,11 +1,20 @@
 package uk.gov.hmrc.perftests.ars
 
 import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
+import uk.gov.hmrc.perftests.alf.AddressLookupFrontendRequests.{lookupAddressFrontendConfirmAddress, lookupAddressFrontendConfirmManualAddress, lookupAddressFrontendGetCsrfToken, lookupAddressFrontendLookupPage, lookupAddressFrontendManualAddress, lookupAddressFrontendSelectAddress, lookupAddressFrontendSelectFirstAddress, lookupAddressFrontendStartJourney, lookupAddressFrontendSubmitManualAddress}
 import uk.gov.hmrc.perftests.ars.AddressLookupRequests._
 
 class AddressLookupSimulation extends PerformanceTestRunner {
 
   setup("lookup", "Lookup address with filter") withRequests (lookupAddressWithFilter)
+
+  setup("lookup-frontend-manual", "Lookup address frontend (Manual lookup)") withRequests (lookupAddressFrontendGetCsrfToken, lookupAddressFrontendStartJourney,
+    lookupAddressFrontendLookupPage, lookupAddressFrontendManualAddress,
+    lookupAddressFrontendSubmitManualAddress, lookupAddressFrontendConfirmManualAddress)
+
+  setup("lookup-frontend", "Lookup address frontend") withRequests (lookupAddressFrontendGetCsrfToken, lookupAddressFrontendStartJourney,
+                                                                                    lookupAddressFrontendLookupPage, lookupAddressFrontendSelectAddress,
+                                                                                    lookupAddressFrontendSelectFirstAddress, lookupAddressFrontendConfirmAddress)
 
   runSimulation()
 }
